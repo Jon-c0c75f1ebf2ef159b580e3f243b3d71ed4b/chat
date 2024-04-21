0bbf05df4a6c47b6a4d58621da49707498ae4e34
Jon <148043487+Jon-c0c75f1ebf2ef159b580e3f243b3d71ed4b@users.noreply.github.com>
2024-04-22 01:38:40 +0300

    Update User.h

--- User.h
+++ User.h
@@ -5,19 +5,18 @@ using namespace std;
 class User
 {
 public:
-	User(const string& login, const string& password, const string& name) : login_(login), password_(password), name_(name) {}
+	User(const string& login, const size_t& password, const string& name) : login_(login), password_(password), name_(name) {}
 	const string& getlog() const { return login_; }
 	const string& getName() const { return name_; }
-	const string& getPass() const { return password_; }
+	const size_t& getPass() const { return password_; }
 
 	void setName(const string& name) { name_ = name; }
-	void setPass(const string& password) { password_ = password; }
+	void setPass(const size_t& password) { password_ = password; }
 
 
 private:
-	string login_;
-	string password_;
+	string login_;      //login is salt for hash of the password
+	size_t password_;   //hash of the salt and password
 	string name_;
 
 };
-
