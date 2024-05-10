48d792db09ba1d8a4d2a2ad3ba0dd04fa78bc9e1
Jon <148043487+Jon-c0c75f1ebf2ef159b580e3f243b3d71ed4b@users.noreply.github.com>
2024-05-10 03:59:47 +0300

    Update Hashing.h

--- Hashing.h
+++ Hashing.h
@@ -6,13 +6,13 @@ class Hashing
 {
 public:
     std::size_t hashing(std::string input_pas, std::string input_salt) {
-    
+
         std::string sait_pas = input_pas + input_salt;
-        
+
         std::hash<std::string> hasher;
-        
-        std::size_t output_hash = hasher(input_pas);
-        
+
+        std::size_t output_hash = hasher(sait_pas);
+
         return output_hash;
     };
 };
